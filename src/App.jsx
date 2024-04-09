273521de1bb9a11a70a9896eaf2edd33b53d0258
Alexandre78R <alexandre.renard98@gmail.com>
2024-04-09 15:06:32 +0200

    add code bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,49 @@
+import useState from "react";
 import "./App.css";
-import Categories from "./components/Categories/Categories";
+import Category from "./components/Categories/Categories";
 
 function App() {
-  const categories = [
-    { name: "Fruit"},
-    { name:"Vegetable"}
-  ];
 
-  const articles = [
-    { name: "Apple", category: "Fruit" },
-    { name: "Banana", category: "Fruit" },
-    { name: "Orange", category: "Fruit" },
-    { name: "Strawberry", category: "Fruit" },
-    { name: "Grapes", category: "Fruit" },
-    { name: "Watermelon", category: "Fruit" },
-    { name: "Pineapple", category: "Fruit" },
-    { name: "Kiwi", category: "Fruit" },
-    { name: "Mango", category: "Fruit" },
-    { name: "Peach", category: "Fruit" },
-    { name: "Pear", category: "Fruit" },
-    { name: "Plum", category: "Fruit" },
-    { name: "Cherry", category: "Fruit" },
-    { name: "Carrot", category: "Vegetable" },
-    { name: "Broccoli", category: "Vegetable" },
-    { name: "Tomato", category: "Vegetable" },
-    { name: "Lettuce", category: "Vegetable" },
-    { name: "Cucumber", category: "Vegetable" },
-    { name: "Potato", category: "Vegetable" },
-    { name: "Onion", category: "Vegetable" },
-    { name: "Garlic", category: "Vegetable" },
-    { name: "Bell pepper", category: "Vegetable" },
-    { name: "Spinach", category: "Vegetable" },
-    { name: "Zucchini", category: "Vegetable" },
-    { name: "Eggplant", category: "Vegetable" },
-    { name: "Cauliflower", category: "Vegetable" },
-  ];
+  //State à changer pour savoir si on a bien récupérer les données de l'api
+  const [loading, setLoading] = useState(false);
+
+  // State du tableau de données des catégories
+  const [categories, setCategories] = useState([]);
+
+  // State du tableau de données des articles
+  const [articles, setArticles] = useState(null);
+
+  const handleClick = () => {
+    // Obtenez les données via l'API en utilisant les liens suivants : http://localhost:3000
+    fetch("http://localhost:3000/categories")
+    .then(data => setCategories(data))
+    .catch(err => console.log(err))
+
+    fetch("/articles")
+    .then(response => response.json())
+    .then(data => setArticles([]))
+    .catch(err => console.log(err))
+
+
+    setLoading(true)
+  }
 
   return (
     <>
-      {categories.map((category, index) => {
-        // Solution 1
-        return (
-          <Categories
-            key={index}
-            category={category}
-            articles={articles.filter(
-              (article) => category.name === article.category
-            )}
-          />
-        );
-        // Solution 2
-        // return (
-        //   <Categories key={index} category={category} articles={articles} />
-        // );
-      })}
+      <button onClick={handleClick}>{loading ? "Mettre à jour les catégories" : "Voir les catégories"}</button>
+      {
+          categories.map((category, index) => {
+            return (
+              <Categories
+                key={index}
+                category={category}
+                articles={articles.filter(
+                  (article) => category.name === article.category
+                )}
+              />
+            );
+          })
+      }
     </>
   );
 }
--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -2,38 +2,22 @@ import { useState } from "react";
 import Articles from "../Articles/Articles";
 
 function Categories(props) {
-  console.log("props", props)
   const [isVisibleCategory, setIsVisibleCategory] = useState(false);
 
   const visibleCategory = () => {
-    //Solution 1
+
     setIsVisibleCategory(!isVisibleCategory);
-    // Solution 2
-    // if (isVisibleCategory) {
-    //   setIsVisibleCategory(false);
-    // } else {
-    //   setIsVisibleCategory(true);
-    // }
   };
 
-  // Solution 2
-  // const filterArticle = props.articles.filter(
-  //   (article) => props.category === article.category
-  // );
 
   return (
     <>
       <h1 onClick={visibleCategory}>{props.category.name}</h1>
       {isVisibleCategory && (
         <>
-          {/* Solution 1 */}
           {props.articles.map((article, index) => (
             <Articles key={index} article={article} />
           ))}
-          {/* Solution 2  */}
-          {/* {filterArticle.map((article, index) => (
-            <Articles key={index} article={article} />
-          ))} */}
         </>
       )}
     </>
