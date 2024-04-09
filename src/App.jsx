3c7b7bab94f017c0114754c86b6bad1d5bce0e1a
Alexandre78R <alexandre.renard98@gmail.com>
2024-04-09 15:08:09 +0200

    correction atelier

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [loading, setLoading] = useState(false);
 
   // State du tableau de données des catégories
-  const [categories, setCategories] = useState(null);
+  const [categories, setCategories] = useState([]);
 
   // State du tableau de données des articles
   const [articles, setArticles] = useState([]);
