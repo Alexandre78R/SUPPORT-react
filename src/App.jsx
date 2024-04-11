f4e2e749d80978a2116501c71b6ec4941a7a57af
Vincent C <vinch.cm@gmail.com>
2024-04-11 09:42:11 +0200

    support done

--- src/App.jsx
+++ src/App.jsx
@@ -5,43 +5,16 @@ import FetchData from "./components/FetchData/FetchData";
 function App() {
   const [data, setData] = useState(null);
 
-  // Solution 1
-  // function getData() {
-  //   fetch(`https://swapi.dev/api/people`)
-  //     .then((response) => response.json())
-  //     .then((data) => setData(data.results))
-  //     .catch((err) => console.error(err));
-  // }
-
-  // Solution 2
-  const getData = () => {
-    axios.get(`https://swapi.dev/api/people`).then((response) => {
-      setData(response.data.results);
-    });
-  };
-
   // url to fetch (with or without axios, as you wish): `https://swapi.dev/api/people`
 
   // data to get and to send via props: name, heigth, gender in priority, and films if you want to try
 
-  // Component FetchData to create to receive and display the props - don't forget to import the Component in App.jsx
+  // Component FetchData to create to receive and display the props - already imported in App:)
 
   // In App.jsx, get data with the button - make sure that data exist before mapping !
 
   return (
     <div>
-      {data &&
-        data.map((perso, index) => {
-          return (
-            <FetchData
-              key={index}
-              name={perso.name}
-              height={perso.height}
-              gender={perso.gender}
-              films={perso.films}
-            />
-          );
-        })}
       <button type="button" onClick={getData}>
         Get data
       </button>
--- src/components/FetchData/FetchData.jsx
+++ src/components/FetchData/FetchData.jsx
@@ -1,23 +1,5 @@
 import React from "react";
 
-function FetchData(props) {
-  const { name, height, gender, films } = props;
-  return (
-    <div>
-      <p>{name}</p>
-      <p>{height}</p>
-      <p>{gender}</p>
-      <ul>
-        {films.map((film) => (
-          <li key={film}>
-            <a target="_blank" href={film}>
-              {film}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+function FetchData(props) {}
 
 export default FetchData;
