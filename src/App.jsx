ed9175e86e3cf28e0baf32f3d9e87d4c0828d350
Vincent C <vinch.cm@gmail.com>
2024-04-11 09:26:45 +0200

    orders added

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,21 @@ import FetchData from "./components/FetchData/FetchData";
 function App() {
   const [data, setData] = useState(null);
 
-  const getData = () => {
-    axios.get(`https://swapi.dev/api/people`).then((response) => {
-      console.log(response.data.results);
-      setData(response.data.results);
-    });
-  };
+  // url to fetch (with axios or "classic" fetch, as you wish): `https://swapi.dev/api/people`
+
+  // data to get and to send via props: name, heigth, gender in priority, and films if you want to try
+
+  // Component FetchData to create to display the value of the props - don't forget to import the Component in App.jsx
+
+  // In App.jsx, make sure that data exist, and get it with the button "get data"
+
   return (
     <div>
       {data &&
-        data.map((perso) => {
+        data.map((perso, index) => {
           return (
             <FetchData
+              key={index}
               name={perso.name}
               height={perso.height}
               gender={perso.gender}
