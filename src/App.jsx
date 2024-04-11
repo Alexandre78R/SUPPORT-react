57c8887dd29a92e2b79ecf673ccb1c9054091caf
Vincent C <vinch.cm@gmail.com>
2024-04-11 10:27:46 +0200

    correction added

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,35 @@ import FetchData from "./components/FetchData/FetchData";
 function App() {
   const [data, setData] = useState(null);
 
-  const getData = () => {};
+  // Solution 1
+  // function getData() {
+  //   fetch(`https://swapi.dev/api/people`)
+  //     .then((response) => response.json())
+  //     .then((data) => setData(data.results))
+  //     .catch((err) => console.error(err));
+  // }
+
+  // Solution 2 with axios
+  const getData = () => {
+    axios.get(`https://swapi.dev/api/people`).then((response) => {
+      setData(response.data.results);
+    });
+  };
 
   return (
     <div>
+      {data &&
+        data.map((perso, index) => {
+          return (
+            <FetchData
+              key={index}
+              name={perso.name}
+              height={perso.height}
+              gender={perso.gender}
+              films={perso.films}
+            />
+          );
+        })}
       <button type="button" onClick={getData}>
         Get data
       </button>
--- src/components/FetchData/FetchData.jsx
+++ src/components/FetchData/FetchData.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
-function FetchData() {}
+function FetchData(props) {
+  const { name, height, gender, films } = props;
+  return (
+    <div>
+      <p>{name}</p>
+      <p>{height}</p>
+      <p>{gender}</p>
+      <ul>
+        {films.map((film) => (
+          <li key={film}>
+            <a target="_blank" href={film}>
+              {film}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default FetchData;
