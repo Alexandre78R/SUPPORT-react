7a9781e07365753fb42f215fb9ee1e7cbe2e7c68
Vincent C <vinch.cm@gmail.com>
2024-04-12 09:31:46 +0200

    correction redone with map in both components

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,33 @@ function App() {
   const [data, setData] = useState(null);
 
   // Solution 1
-  // function getData() {
-  //   fetch(`https://swapi.dev/api/people`)
-  //     .then((response) => response.json())
-  //     .then((data) => setData(data.results))
-  //     .catch((err) => console.error(err));
-  // }
+  function getData() {
+    fetch(`https://swapi.dev/api/people`)
+      .then((response) => response.json())
+      .then((data) => setData(data.results))
+      .catch((err) => console.error(err));
+  }
 
   // Solution 2 with axios
-  const getData = () => {
-    axios.get(`https://swapi.dev/api/people`).then((response) => {
-      setData(response.data.results);
-    });
-  };
+  // const getData = () => {
+  //   axios.get(`https://swapi.dev/api/people`).then((response) => {
+  //     setData(response.data.results);
+  //   });
+  // };
 
   return (
     <>
-      {data ? (
-        <FetchData data={data} />
-      ) : (
-        <div id="buttonArea">
-          <button type="button" onClick={getData}>
-            Get data
-          </button>
-        </div>
-      )}
+      <div className="persoCards">
+        {data ? (
+          data.map((perso, index) => <FetchData key={index} perso={perso} />)
+        ) : (
+          <div id="buttonArea">
+            <button type="button" onClick={getData}>
+              Get data
+            </button>
+          </div>
+        )}
+      </div>
     </>
   );
 }
--- src/components/FetchData/FetchData.jsx
+++ src/components/FetchData/FetchData.jsx
@@ -1,25 +1,20 @@
 import React from "react";
 import "./FetchData.css";
 
-function FetchData({ data }) {
+function FetchData({ perso }) {
   return (
     <>
-      <div id="persoCards">
-        {data.map((perso, index) => (
-          <section class="persoCard" key={index}>
-            <h1>{perso.name}</h1>
-            <p>{perso.height}</p>
-            <ul>
-              {perso.films.map((film, index) => (
-                <a href={film} target="_blank">
-                  <li key={index}>{film}</li>
-                </a>
-              ))}
-            </ul>
-          </section>
-        ))}
-        ;
-      </div>
+      <section className="persoCard">
+        <h1>{perso.name}</h1>
+        <p>{perso.height}</p>
+        <ul>
+          {perso.films.map((film, index) => (
+            <a key={index} href={film} target="_blank">
+              <li>{film}</li>
+            </a>
+          ))}
+        </ul>
+      </section>
     </>
   );
 }
